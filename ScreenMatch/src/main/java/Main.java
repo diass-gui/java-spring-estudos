bf5ee3e374875624b99cc4e4c5110436bab759f1
diass-gui <enriqueguilherme2@gmail.com>
2026-07-21 23:56:27 -0300

    Upload: Primeiras anotações do curso 2 iniciadas. Curso voltado à programação orientada a objetos.

---
+++ ScreenMatch/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancamento);
+    }
+}
