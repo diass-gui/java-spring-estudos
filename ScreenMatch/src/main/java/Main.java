0aa673291bfec9290ab13e0a987dc644a9dfbb8d
diass-gui <enriqueguilherme2@gmail.com>
2026-07-23 13:05:39 -0300

    Update: anotações sobre herança, polimorfismo e sobrescrita de métodos

--- ScreenMatch/src/main/java/Main.java
+++ ScreenMatch/src/main/java/Main.java
@@ -1,9 +1,10 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.model.Filme;
+import br.com.alura.screenmatch.model.Serie;
 
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-
         meuFilme.setNome("The Matrix");
         meuFilme.setAnoDeLancamento(1999);
         meuFilme.setDuracaoEmMinutos(135);
@@ -14,5 +15,26 @@ public class Main {
         meuFilme.avalia(9);
         meuFilme.avalia(10);
         System.out.println("Média das avaliações do filme: " + meuFilme.retornaMedia());
+
+        Serie serie = new Serie();
+        serie.setNome("Lost");
+        serie.setAnoDeLancamento(2000);
+        serie.exibeFichaTecnica();
+        serie.setTemporadas(10);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(50);
+        System.out.println("Duração da maratona de Lost: " + serie.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+        outroFilme.setIncluidoNoPlano(true);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(serie);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
---
+++ ScreenMatch/src/main/java/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.model.Filme;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.Titulo;
+
+public class CalculadoraDeTempo {
+
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+//    public void inclui(Filme f) {
+//        this.tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie serie) {
+//        this.tempoTotal += serie.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+
+}
